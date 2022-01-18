c599d969ab8fb6be4f1b4d9b1ee5ac68d51822f8
SungHoon(PC) <610ksh@naver.com>
2022-01-19 02:22:36 +0900

    120강까지 완료. (Gun Shooting Mode)

--- TheLastRPG/Source/TheLastRPG/Private/SH/SH_CRifle.cpp
+++ TheLastRPG/Source/TheLastRPG/Private/SH/SH_CRifle.cpp
@@ -1,6 +1,16 @@
 #include "SH/SH_CRifle.h"
 #include "SH/SH_Global.h"
+#include "Animation/AnimMontage.h"
+#include "GameFramework/Character.h"
 #include "Components/SkeletalMeshComponent.h"
+
+ASH_CRifle* ASH_CRifle::Spawn(class UWorld* InWorld, class ACharacter* InOwner)
+{
+	FActorSpawnParameters params;
+	params.Owner = InOwner;
+
+	return InWorld->SpawnActor<ASH_CRifle>(params);
+}
 
 ASH_CRifle::ASH_CRifle()
 {
@@ -8,16 +18,36 @@ ASH_CRifle::ASH_CRifle()
 
 	SH_CHelpers::CreateComponent<USkeletalMeshComponent>(this, &Mesh, "Mesh");
 
-
 	USkeletalMesh* mesh;
-	SH_CHelpers::GetAsset<USkeletalMesh>(&mesh, "SkeletalMesh'/Game/ParagonCountess/Characters/Heroes/Countess/Meshes/SM_Countess.SM_Countess'");
+	SH_CHelpers::GetAsset<USkeletalMesh>(&mesh, "SkeletalMesh'/Game/data/119/Weapons/Meshes/AR4/SK_AR4.SK_AR4'");
 	Mesh->SetSkeletalMesh(mesh);
+
+	/// TODO Montages
+	SH_CHelpers::GetAsset<UAnimMontage>(&GrabMontage, "AnimMontage'/Game/SungHoon/Montages/SH_Rifle_Grab_Montage.SH_Rifle_Grab_Montage'");
+	SH_CHelpers::GetAsset<UAnimMontage>(&UngrabMontage, "AnimMontage'/Game/SungHoon/Montages/SH_Rifle_Ungrab_Montage.SH_Rifle_Ungrab_Montage'");
+
+}
+
+void ASH_CRifle::Equip()
+{
+}
+
+void ASH_CRifle::Begin_Equip()
+{
+}
+
+void ASH_CRifle::End_Equip()
+{
 }
 
 void ASH_CRifle::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	OwnerCharacter = Cast<ACharacter>(GetOwner()); // 본인 자신의 소유권을 가져옴
+												  
+	// 본인 캐릭터의 메시로가서 소켓에 현재 본인을 부착하는 함수.
+	AttachToComponent(OwnerCharacter->GetMesh(), FAttachmentTransformRules(EAttachmentRule::KeepRelative, true), HolsterSocket);
 }
 
 void ASH_CRifle::Tick(float DeltaTime)
--- TheLastRPG/Source/TheLastRPG/Public/SH/SH_CRifle.h
+++ TheLastRPG/Source/TheLastRPG/Public/SH/SH_CRifle.h
@@ -14,8 +14,32 @@ class THELASTRPG_API ASH_CRifle : public AActor
 private:
 	UPROPERTY(VisibleDefaultsOnly, Category = "Rifle")
 		class USkeletalMeshComponent* Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Rifle")
+		FName HandSocket = "Hand_Rifle";
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Rifle")
+		FName HolsterSocket = "Holster_Rifle";
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Rifle")
+		class UAnimMontage* GrabMontage;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Rifle")
+		class UAnimMontage* UngrabMontage;
+
+public:
+	// 자기 자료형을 생성해서 리턴함. static이라 독립적임. 월드와 캐릭터를 받아준다.
+	static ASH_CRifle* Spawn(class UWorld* InWorld, class ACharacter* InOwner);
+
+public:
+	FORCEINLINE bool GetEquipped() { return bEquipped; }
+
 public:	
 	ASH_CRifle();
+
+	void Equip();
+	void Begin_Equip();
+	void End_Equip();
 
 protected:
 	virtual void BeginPlay() override;
@@ -23,4 +47,8 @@ protected:
 public:	
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	class ACharacter* OwnerCharacter;
+
+	bool bEquipped;
 };
